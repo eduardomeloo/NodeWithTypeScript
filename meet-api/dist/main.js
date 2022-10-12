c2e4a5ec4a794f246c7a51910d5760fae74551ee
Eduardo <eduardo.peixoto.melo@hotmail.com>
2022-10-12 10:42:53 -0300

    First API with Restify

--- meet-api/dist/main.js
+++ meet-api/dist/main.js
@@ -1,1 +1,17 @@
-console.log('Funcionou...2');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//const restify = require('restify')
+const restify = require("restify");
+const server = restify.createServer({
+    name: 'meet-api',
+    version: '1.0.0'
+});
+server.get('/hello', (req, res, next) => {
+    res.json({
+        message: 'hello'
+    });
+    return next();
+});
+server.listen(3000, () => {
+    console.log('API is runnig on http://localhost:3000');
+});
--- meet-api/main.ts
+++ meet-api/main.ts
@@ -1,1 +1,18 @@
-console.log('Funcionou...2')
+//const restify = require('restify')
+import * as restify from'restify'
+
+const server = restify.createServer({
+    name: 'meet-api',
+    version: '1.0.0'
+})
+
+server.get('/hello', (req, res, next) => {
+    res.json({
+        message:'hello'
+    })
+    return next()
+})
+
+server.listen(3000, () => {
+    console.log('API is runnig on http://localhost:3000')
+})
