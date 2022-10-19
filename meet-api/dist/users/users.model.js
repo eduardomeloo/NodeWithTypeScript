f4c190bb4adf91bd248a0b4c51fed2ea15d01319
Eduardo <eduardo.peixoto.melo@hotmail.com>
2022-10-19 20:21:16 -0300

    Implementação do método findById e criação de uma nova rota

--- meet-api/dist/users/users.model.js
+++ meet-api/dist/users/users.model.js
@@ -3,10 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 const users = [
     {
+        id: '1',
         name: 'Peter Parker',
         email: 'peter@marvel.com'
     },
     {
+        id: '2',
         name: 'Bruce Wayne',
         email: 'bruce@dc.com'
     }
@@ -14,6 +16,16 @@ const users = [
 class User {
     static findAll() {
         return Promise.resolve(users);
+    }
+    static findById(id) {
+        return new Promise(resolve => {
+            const filtered = users.filter(user => user.id === id);
+            let user = undefined;
+            if (filtered.length > 0) {
+                user = filtered[0];
+            }
+            resolve(user);
+        });
     }
 }
 exports.User = User;
