9e0fa62159100626044760e253ca8c2a2bf83569
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-11-06 23:07:27 -0300

    Refactory functions and validate ObjectId
    Refactory functions and validate ObjectId

--- meet-api/dist/users/users.router.js
+++ meet-api/dist/users/users.router.js
@@ -1,73 +1,23 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.usersRouter = void 0;
-const router_1 = require("../common/router");
-const restify_errors_1 = require("restify-errors");
+const model_router_1 = require("../common/model-router");
 const users_model_1 = require("./users.model");
-class UsersRouter extends router_1.Router {
+class UsersRouter extends model_router_1.ModelRouter {
     constructor() {
-        super();
+        super(users_model_1.User);
         this.on('beforeRender', document => {
             document.password = undefined;
             //delete document.password
         });
     }
     applyRoutes(application) {
-        application.get('/users', (req, resp, next) => __awaiter(this, void 0, void 0, function* () {
-            users_model_1.User.find()
-                .then(this.render(resp, next))
-                .catch(next);
-        }));
-        application.get('/users/:id', (req, resp, next) => {
-            users_model_1.User.findById(req.params.id)
-                .then(this.render(resp, next))
-                .catch(next);
-        });
-        application.post('/users', (req, resp, next) => {
-            let user = new users_model_1.User(req.body); //Cria um novo documento vazio
-            user.save()
-                .then(this.render(resp, next))
-                .catch(err => next(err));
-        });
-        application.put('/users/:id', (req, resp, next) => __awaiter(this, void 0, void 0, function* () {
-            const options = { runValidators: true, overwrite: true, returnDocument: 'after' };
-            users_model_1.User.findOneAndUpdate({ _id: req.params.id }, req.body, options)
-                .then(result => {
-                if (result._id) {
-                    return users_model_1.User.findById(req.params.id);
-                }
-                else {
-                    throw new restify_errors_1.NotFoundError('Documento não encontrado');
-                }
-            }).then(this.render(resp, next))
-                .catch(next);
-        }));
-        application.patch('/users/:id', (req, resp, next) => {
-            const options = { new: true, runValidators: true };
-            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then(this.render(resp, next))
-                .catch(next);
-        });
-        application.del('/users/:id', (req, resp, next) => {
-            users_model_1.User.deleteOne({ _id: req.params.id }).exec().then(cmdResult => {
-                if (cmdResult.deletedCount) {
-                    resp.send(204);
-                }
-                else {
-                    throw new restify_errors_1.NotFoundError('Documento não encontrado');
-                }
-                return next();
-            }).catch(next);
-        });
+        application.get('/users', this.findAll);
+        application.get('/users/:id', [this.validateId, this.findById]);
+        application.post('/users', this.save);
+        application.put('/users/:id', [this.validateId, this.replace]);
+        application.patch('/users/:id', [this.validateId, this.update]);
+        application.del('/users/:id', [this.validateId, this.delete]);
     }
 }
 exports.usersRouter = new UsersRouter();
--- meet-api/users/users.router.ts
+++ meet-api/users/users.router.ts
@@ -1,12 +1,12 @@
-import {Router} from '../common/router'
+import {ModelRouter} from '../common/model-router'
 import * as restify from 'restify'
 import { NotFoundError } from 'restify-errors'
 import {User} from './users.model'
 
-class UsersRouter extends Router {
+class UsersRouter extends ModelRouter<User> {
 
     constructor() {
-        super()
+        super(User)
         this.on('beforeRender', document => {
             document.password = undefined
             //delete document.password
@@ -14,58 +14,12 @@ class UsersRouter extends Router {
     }
 
     applyRoutes(application: restify.Server){
-        application.get('/users', async (req, resp, next) => {
-            User.find()
-                .then(this.render(resp, next))
-                .catch(next)
-
-        })
-
-        application.get('/users/:id', (req, resp, next) => {
-            User.findById(req.params.id)
-                .then(this.render(resp, next))
-                .catch(next)
-        })
-
-        application.post('/users', (req, resp, next) => {
-            let user = new User(req.body) //Cria um novo documento vazio
-            user.save()
-                .then(this.render(resp, next))
-                .catch(err => next(err))
-        })
-
-        application.put('/users/:id', async (req, resp, next) => {
-            const options = {runValidators: true, overwrite: true, returnDocument: 'after'}
-            User.findOneAndUpdate({_id : req.params.id}, req.body, options)
-                .then(result =>{
-                   
-                    if(result._id){
-                        return User.findById(req.params.id)
-                    } else {
-                        throw new NotFoundError('Documento não encontrado')
-                    }
-                    
-                }).then(this.render(resp, next))
-                  .catch(next)
-        })
-
-        application.patch('/users/:id', (req, resp, next) => {
-            const options = { new : true, runValidators: true }
-            User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then(this.render(resp, next))
-                .catch(next)
-        })
-
-        application.del('/users/:id', (req, resp, next) => {
-            User.deleteOne({_id: req.params.id}).exec().then(cmdResult => {
-                if(cmdResult.deletedCount) {
-                    resp.send(204)
-                } else {
-                    throw new NotFoundError('Documento não encontrado')
-                }
-                return next()
-            }).catch(next)
-        })
+        application.get('/users', this.findAll)
+        application.get('/users/:id', [this.validateId, this.findById])
+        application.post('/users', this.save)
+        application.put('/users/:id', [this.validateId,this.replace])
+        application.patch('/users/:id', [this.validateId,this.update])
+        application.del('/users/:id', [this.validateId,this.delete])
     }
 }
 
