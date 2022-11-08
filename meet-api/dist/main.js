31078cd49080c6790a1d89e5d1ce4446268f70f7
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-11-07 21:34:48 -0300

    Implements API Restaurants

--- meet-api/dist/main.js
+++ meet-api/dist/main.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = require("./server/server");
 const users_router_1 = require("./users/users.router");
+const restaurants_router_1 = require("./restaurants/restaurants.router");
 const server = new server_1.Server();
-server.bootstrap([users_router_1.usersRouter]).then(server => {
+server.bootstrap([
+    users_router_1.usersRouter,
+    restaurants_router_1.restaurantsRouter
+]).then(server => {
     console.log('API is runnig on:', server.application.address());
 }).catch(error => {
     console.log('Server failed to start');
+    console.error(error);
     process.exit(1); //saída anormal
 });
--- meet-api/main.ts
+++ meet-api/main.ts
@@ -1,10 +1,15 @@
 import {Server} from './server/server'
 import {usersRouter} from './users/users.router'
+import { restaurantsRouter } from './restaurants/restaurants.router'
 const server = new Server()
 
-server.bootstrap([usersRouter]).then(server => {
+server.bootstrap([
+    usersRouter,
+    restaurantsRouter
+]).then(server => {
     console.log('API is runnig on:' , server.application.address())
 }).catch(error => {
     console.log('Server failed to start')
+    console.error(error)
     process.exit(1) //saída anormal
 })
