a090dfdd38a68535b340c6b35b91c585a1d2c467
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-11-15 20:54:07 -0300

    test method patch on api users

--- meet-api/users/users.test.ts
+++ meet-api/users/users.test.ts
@@ -4,6 +4,7 @@ import { Server } from '../server/server'
 import { environment } from '../common/environment'
 import { usersRouter } from './users.router'
 import { User } from './users.model'
+import { response } from 'express'
 
 let address: string
 let server: Server
@@ -29,17 +30,46 @@ test('post /users', async () => {
     const response = await request(address)
         .post('/users')
         .send({
-            name: 'Junesco',
-            email: 'junesco@josefino.com',
+            name: 'usuario1',
+            email: 'usuario1@email.com',
             password: '123456',
             cpf: '756.581.158-09'
         })
     expect(response.status).toBe(200);
     expect(response.body._id).toBeDefined()
-    expect(response.body.name).toBe('Junesco')
-    expect(response.body.email).toBe('junesco@josefino.com')
+    expect(response.body.name).toBe('usuario1')
+    expect(response.body.email).toBe('usuario1@email.com')
     expect(response.body.cpf).toBe('756.581.158-09')
     expect(response.body.password).toBeUndefined()
+})
+
+test('get /users/aaaaa - not found', async () => {
+    const response = await request(address)
+        .get('/users/aaaaa')
+    expect(response.status).toBe(404);
+})
+
+test('patch /users/:id', async () => {
+    return request(address)
+        .post('/users')
+        .send({
+            name: 'usuario2',
+            email: 'usuario2@email.com',
+            password: '123456'
+        })
+        .then(res => request(address)
+                    .patch(`/users/${res.body._id}`)
+                    .send({
+                        name: 'usuario2 - patch'
+                    })
+        )
+        .then(response => {
+            expect(response.status).toBe(200);
+            expect(response.body._id).toBeDefined()
+            expect(response.body.name).toBe('usuario2 - patch')
+            expect(response.body.email).toBe('usuario2@email.com')
+            expect(response.body.password).toBeUndefined()  
+        })  
 })
 
 afterAll(() => {
