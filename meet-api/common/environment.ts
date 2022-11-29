1ce913b757f2b9dfb807e6a12c1b9e4269b89aee
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-11-28 23:13:55 -0300

    ajuste

--- meet-api/common/environment.ts
+++ meet-api/common/environment.ts
@@ -1,3 +1,4 @@
+require('dotenv').config()
 export const environment = {
     server : { port: process.env.SERVER_PORT || 3000 },
     db: { 
--- meet-api/main.ts
+++ meet-api/main.ts
@@ -2,6 +2,8 @@ import {Server} from './server/server'
 import {usersRouter} from './users/users.router'
 import { restaurantsRouter } from './restaurants/restaurants.router'
 import {reviewsRouter} from './reviews/reviews.router'
+require('dotenv').config()
+
 const server = new Server()
 
 server.bootstrap([
